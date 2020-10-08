2ce136022da20c73bc92babc10288b559b736438
yury <fivestarter@gmail.com>
2020-10-08 14:36:56 +0300

    refactoring

--- src/main/java/ru/fivestarter/demo/AuthorizationSubWindow.java
+++ src/main/java/ru/fivestarter/demo/AuthorizationSubWindow.java
@@ -1,11 +1,12 @@
 package ru.fivestarter.demo;
 
 import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.Resource;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
-import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
@@ -30,39 +31,53 @@ public class AuthorizationSubWindow extends Window {
     }
 
     private void initElements() {
-        loginField = new TextField("Login");
-        loginField.setIcon(VaadinIcons.USER);
-        loginField.setRequiredIndicatorVisible(true);
-
-        passwordField = new PasswordField("Password");
-        passwordField.setIcon(VaadinIcons.PASSWORD);
-        passwordField.setRequiredIndicatorVisible(true);
+        loginField = initFormField("Login", VaadinIcons.USER);
+        passwordField = initFormField("Password", VaadinIcons.PASSWORD);
 
         authorizeButton = new Button("Authorize");
         cancelButton = new Button("Cancel");
     }
 
-    private void initListeners() {
-        authorizeButton.addClickListener(event ->
-                Notification.show("This is the authorize information:",
-                        String.format("Login: %s, Password: %s", loginField.getValue(), passwordField.getValue()),
-                        Notification.Type.HUMANIZED_MESSAGE));
+    private TextField initFormField(String caption, Resource icon) {
+        final TextField textField = new TextField(caption);
+        textField.setIcon(icon);
+        textField.setRequiredIndicatorVisible(true);
+        return textField;
+    }
 
-        cancelButton.addClickListener(event -> {
-            loginField.clear();
-            passwordField.clear();
-            close();
-        });
+    private void initListeners() {
+        authorizeButton.addClickListener(event -> authorize());
+        cancelButton.addClickListener(event -> closeWindow());
+    }
+
+    private void closeWindow() {
+        loginField.clear();
+        passwordField.clear();
+        close();
+    }
+
+    private void authorize() {
+        Notification.show("This is the authorize information:",
+                String.format("Login: %s, Password: %s", loginField.getValue(), passwordField.getValue()),
+                Notification.Type.HUMANIZED_MESSAGE);
     }
 
     private void buildLayout() {
         final VerticalLayout mainLayout = new VerticalLayout();
-        final FormLayout authorizationForm = new FormLayout();
-        final HorizontalLayout buttonsLayout = new HorizontalLayout();
+        mainLayout.setDefaultComponentAlignment(Alignment.BOTTOM_CENTER);
 
-        mainLayout.addComponents(authorizationForm, buttonsLayout);
-        authorizationForm.addComponents(loginField, passwordField);
-        buttonsLayout.addComponents(authorizeButton, cancelButton);
+        mainLayout.addComponents(
+                buildAuthorizationForm(),
+                new HorizontalLayout(authorizeButton, cancelButton));
+
         setContent(mainLayout);
     }
+
+    private FormLayout buildAuthorizationForm() {
+        final FormLayout authorizationForm = new FormLayout();
+        authorizationForm.setWidthUndefined();
+        authorizationForm.addComponents(loginField, passwordField);
+        return authorizationForm;
+    }
+
 }
